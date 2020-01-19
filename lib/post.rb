492cd392642e8ad8e6c74d99708382fc13285a60
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-19 23:18:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,14 @@
 class Post
   
-  attr_accessor :title
+  attr_accessor :title, :author
   
   def initialize
     @title = title
+    @author = author 
+  end
+  
+  def self.author
+    .author
   end
   
 end
